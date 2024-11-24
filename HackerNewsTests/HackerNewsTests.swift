3c8803209be24dafeb039ac6a4fe322a53e0228c
Joe Diragi <joseph.diragi@icloud.com>
2024-11-24 10:19:32 -0500

    Initial Commit

---
+++ HackerNewsTests/HackerNewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsTests.swift
+//  HackerNewsTests
+//
+//  Created by Joe Diragi on 11/24/24.
+//
+
+import Testing
+@testable import HackerNews
+
+struct HackerNewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
